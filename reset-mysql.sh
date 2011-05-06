b6ec6bbd31e7cd0b03f3b35f61cc16b7e64694a8
Carl Meyer <carl@oddbird.net>
2011-05-06 12:45:24 -0500

    Fixed reset-mysql.sh for OSX mktemp.

--- reset-mysql.sh
+++ reset-mysql.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tmpsql=`mktemp`
+tmpsql=`mktemp tmp.XXXXXX`
 cat utest-persistence/src/main/resources/db_scripts/db_tcm_create_empty_db_script.sql | grep -v "GRANT ALL PRIVILEGES" > $tmpsql
 cat utest-persistence/src/main/resources/db_scripts/db_tcm_update_db_script_?.sql >> $tmpsql
 cat utest-persistence/src/main/resources/db_scripts/db_tcm_update_db_script_??.sql >> $tmpsql
